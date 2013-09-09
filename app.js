9e83509bc7bdfd6c943525453888d0ff9e8a829a
Matthew Casperson <matthewcasperson@gmail.com>
2013-09-09 12:23:59 +1000

    Working on building the html pages

--- app.js
+++ app.js
@@ -77,80 +77,80 @@ var indexHtml = null;
 function buildBooks(updatedSpecs, allSpecsArray) {
 	if (specsProcessed && topicsProcessed) {
 
-		indexHtml = "<html>\
-			<head>\
-				<title>Docbuilder Index</title>\
-				<link rel=\"stylesheet\" href=\"index.css\"/>\
-				<script src=\"http://yui.yahooapis.com/3.10.0/build/yui/yui-min.js\"></script>\
-				<script src=\"functions-1.1.js\" ></script>\
-			</head>\
-			<body onload=\"setLangSelectLanguage()\">\
-				<div class=\"container\">\
-					<div class=\"langBar\">Language:\
-						<select id=\"lang\" class=\"langSelect\" onchange=\"changeLang(this)\">\
-							<option selected value=\"\">English</option>\
-							<option value=\"zh-Hans\">Chinese</option>\
-							<option value=\"fr\">French</option>\
-							<option value=\"de\">German</option>\
-							<option value=\"ja\">Japanese</option>\
-							<option value=\"pt-BR\">Portuguese</option>\
-							<option value=\"es\">Spanish</option>\
-						</select>\
-					</div>\
-					<div class=\"content\">\
-						<div>\
-							<img height=\"87\" src=\"pg.png\" width=\"879\">\
-						</div>\
-						<div style=\"margin-top:1em\">\
-							<p>DocBuilder is a service that automatically rebuilds content specifications as they are created or edited.</p>\
-							<p>Each content spec has three links: a link to the compiled book itself, a link to the build log, and a link to the publican log.</p>\
-							<p>If a book could not be built, first check the build log. This log contains information that may indicate syntax errors in the content specification. You can also view this log to see when the document was last built.</p>\
-							<p>If the build log has no errors, check the publican log. This may indicate some syntax errors in the XML.</p>\
-							<p>The topics in each document include a \"Edit this topic\" link, which will take you to the topic in the CCMS.</p>\
-							<p>To view the latest changes to a document, simply refresh the page.</p><p>Estimated Rebuild Time: ${REBUILD_TIME}</p>\
-						</div>\
-						<div></div>\
-						<div>\
-							<table>\
-								<tr>\
-									<td>\
-										ID Filter\
-									</td>\
-									<td>\
-										<input type=\"text\" id=\"idFilter\" onkeyup=\"save_filter()\">\
-									</td>\
-									<td>\
-										Product Filter\
-									</td>\
-									<td>\
-										<input type=\"text\" id=\"productFilter\" onkeyup=\"save_filter()\">\
-									</td>\
-									<td rowspan=\"2\">\
-										<button onclick=\"reset_filter()\">Reset</button>\
-									</td>\
-								</tr>\
-								<tr>\
-									<td>\
-										Version Filter\
-									</td>\
-									<td>\
-										<input type=\"text\" id=\"versionFilter\" onkeyup=\"save_filter()\">\
-									</td>\
-									<td>\
-										Title Filter\
-									</td>\
-									<td>\
-										<input type=\"text\" id=\"titleFilter\" onkeyup=\"save_filter()\">\
-									</td>\
-								</tr>\
-							</table> \
-							</div>\
-							<div></div>\
-						</div>\
-						</div>\
-						<script>\
-							// build the array that holds the details of the books \
-							var data = [";
+		indexHtml = "<html>\n\
+			<head>\n\
+				<title>Docbuilder Index</title>\n\
+				<link rel=\"stylesheet\" href=\"index.css\"/>\n\
+				<script src=\"http://yui.yahooapis.com/3.10.0/build/yui/yui-min.js\"></script>\n\
+				<script src=\"functions-1.1.js\" ></script>\n\
+			</head>\n\
+			<body onload=\"setLangSelectLanguage()\">\n\
+				<div class=\"container\">\n\
+					<div class=\"langBar\">Language:\n\
+						<select id=\"lang\" class=\"langSelect\" onchange=\"changeLang(this)\">\n\
+							<option selected value=\"\">English</option>\n\
+							<option value=\"zh-Hans\">Chinese</option>\n\
+							<option value=\"fr\">French</option>\n\
+							<option value=\"de\">German</option>\n\
+							<option value=\"ja\">Japanese</option>\n\
+							<option value=\"pt-BR\">Portuguese</option>\n\
+							<option value=\"es\">Spanish</option>\n\
+						</select>\n\
+					</div>\n\
+					<div class=\"content\">\n\
+						<div>\n\
+							<img height=\"87\" src=\"pg.png\" width=\"879\">\n\
+						</div>\n\
+						<div style=\"margin-top:1em\">\n\
+							<p>DocBuilder is a service that automatically rebuilds content specifications as they are created or edited.</p>\n\
+							<p>Each content spec has three links: a link to the compiled book itself, a link to the build log, and a link to the publican log.</p>\n\
+							<p>If a book could not be built, first check the build log. This log contains information that may indicate syntax errors in the content specification. You can also view this log to see when the document was last built.</p>\n\
+							<p>If the build log has no errors, check the publican log. This may indicate some syntax errors in the XML.</p>\n\
+							<p>The topics in each document include a \"Edit this topic\" link, which will take you to the topic in the CCMS.</p>\n\
+							<p>To view the latest changes to a document, simply refresh the page.</p><p>Estimated Rebuild Time: ${REBUILD_TIME}</p>\n\
+						</div>\n\
+						<div></div>\n\
+						<div>\n\
+							<table>\n\
+								<tr>\n\
+									<td>\n\
+										ID Filter\n\
+									</td>\n\
+									<td>\n\
+										<input type=\"text\" id=\"idFilter\" onkeyup=\"save_filter()\">\n\
+									</td>\n\
+									<td>\n\
+										Product Filter\n\
+									</td>\n\
+									<td>\n\
+										<input type=\"text\" id=\"productFilter\" onkeyup=\"save_filter()\">\n\
+									</td>\n\
+									<td rowspan=\"2\">\n\
+										<button onclick=\"reset_filter()\">Reset</button>\n\
+									</td>\n\
+								</tr>\n\
+								<tr>\n\
+									<td>\n\
+										Version Filter\n\
+									</td>\n\
+									<td>\n\
+										<input type=\"text\" id=\"versionFilter\" onkeyup=\"save_filter()\">\n\
+									</td>\n\
+									<td>\n\
+										Title Filter\n\
+									</td>\n\
+									<td>\n\
+										<input type=\"text\" id=\"titleFilter\" onkeyup=\"save_filter()\">\n\
+									</td>\n\
+								</tr>\n\
+							</table> \n\
+							</div>\n\
+							<div></div>\n\
+						</div>\n\
+						</div>\n\
+						<script>\n\
+							// build the array that holds the details of the books \n\
+							var data = [\n";
 
 		finishProcessing = function() {
 			indexHtml += " ];\
