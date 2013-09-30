66cd59e04cd975ff4366f7db1f31021e8932e7d6
Matthew Casperson <mcaspers@redhat.com>
2013-10-01 07:55:52 +1000

    Added script to copy javascript files to deployment locations

--- copy_js_files.sh
+++ copy_js_files.sh
@@ -1,2 +1,2 @@
-cp -y javascript/overlay.js /var/www/html/javascript
-cp -y functions-1.1.js /var/www/html
+cp -f javascript/overlay.js /var/www/html/javascript
+cp -f functions-1.1.js /var/www/html
