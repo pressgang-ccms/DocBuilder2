e3df3b026f864c617cf165cc6502ce1205191187
Matthew Casperson <matthewcasperson@gmail.com>
2013-10-12 06:53:20 +1000

    Added update script

---
+++ git_force_pull.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a convenience script to force git to pull down the latest
+# version of DocBuilder
+
+echo deployment_details.js will be copied to /tmp. Copy it back if you had custom settings that were overwritten by GIT
+
+cp deployment_detail.js /tmp
+
+git fetch --all
+git reset --hard origin/master
