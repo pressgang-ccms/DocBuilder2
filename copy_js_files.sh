96536a8cdc382f5d4cf631afbb280ae7a16b5ce0
Matthew Casperson <mcaspers@redhat.com>
2013-10-25 13:20:39 +1000

    Working on mojo integration

--- copy_js_files.sh
+++ copy_js_files.sh
@@ -1,5 +1,4 @@
 cp -f javascript/overlay.js /var/www/html/javascript
 cp -f functions-1.1.js /var/www/html
 cp -f PressZilla.user.js /var/www/html
-cp -f PressZilla.meta.js /var/www/html
 cp -r presszilla /var/www/html
