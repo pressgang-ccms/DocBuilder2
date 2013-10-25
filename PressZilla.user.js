7a40518b6a0263ec14bed500c0e6dcf35380ca5d
Matthew Casperson <mcaspers@redhat.com>
2013-10-25 12:52:37 +1000

    Working on mojo integration

--- PressZilla.user.js
+++ PressZilla.user.js
@@ -6,19 +6,19 @@
 // @include     https://bugzilla.redhat.com/*
 // @include     http://docbuilder.usersys.redhat.com/*
 // @include     http://docbuilder.ecs.eng.bne.redhat.com/*
+// @include     https://docbuilder.usersys.redhat.com/*
+// @include     https://docbuilder.ecs.eng.bne.redhat.com/*
 // @require     http://code.jquery.com/jquery-2.0.3.min.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaCommonUtils_v2.1.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaKeywordExtraction_v2.1.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaBugzillaIntegration_v2.1.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaSolutionsIntegration_v2.1.4.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaMojoIntegration_v2.1.3.js
-// @version     2.1.5
+// @version     2.1.6
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
 // @downloadURL https://docbuilder.usersys.redhat.com/PressZilla.user.js
-// @updateURL   https://docbuilder.usersys.redhat.com/PressZilla.meta.js
+// @updateURL   https://docbuilder.usersys.redhat.com/PressZilla.user.js
 // @homepageURL https://docbuilder.usersys.redhat.com
 // ==/UserScript==
-
-logToConsole("PressZilla Started");
 
