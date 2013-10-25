55ac8459dcc16734a0667cb60f64b2938d206e08
Matthew Casperson <mcaspers@redhat.com>
2013-10-25 13:40:27 +1000

    Working on jboss.org integration

--- presszilla/PressZillaJbossOrgIntegration_v2.2.1.js
+++ presszilla/PressZillaJbossOrgIntegration_v2.2.1.js
@@ -1,15 +1,15 @@
 /*
- This file contains the logic for searching the KBase for solutions based on the keywords in a topic.
+ This file contains the logic for searching the jboss.org website based on the keywords in a topic.
  */
 (function() {
     if (isDocbuilderWindow()) {
 
-        var mojoCache = {};
+        var cache = {};
 
         function addClickFunction(buttonId, topicId, popoverId) {
             jQuery('#' + buttonId).click(function() {
                 jQuery('#' + buttonId).attr('disabled', 'true');
-                jQuery('#' + buttonId).text('Getting Documents');
+                jQuery('#' + buttonId).text('Getting Content');
                 jQuery('#' + buttonId).removeClass('btn-primary');
                 jQuery('#' + buttonId).removeClass('btn-danger');
                 jQuery('#' + buttonId).addClass('btn-primary');
@@ -36,19 +36,19 @@
          * @param popoverId The popover id
          */
         function getSolutions(topic, topicId, popoverId) {
-            if (!mojoCache[topicId].fetchingDocuments) {
+            if (!cache[topicId].fetchingDocuments) {
 
-                mojoCache[topicId].fetchingDocuments = true;
+                cache[topicId].fetchingDocuments = true;
 
                 var keywords = "";
                 for (var keywordIndex = 0, keywordCount = topic.keywords.length; keywordIndex < keywordCount; ++keywordIndex){
                     if (keywords.length != 0) {
-                        keywords += ",";
+                            keywords += ",";
                     }
                     keywords += topic.keywords[keywordIndex];
                 }
 
-                var kcsUrl = "https://mojo.redhat.com/api/core/v3/search/contents?filter=search(" + encodeURIComponent(keywords) + ")";
+                var kcsUrl = "https://community.jboss.org/api/core/v3/search/contents?filter=search(" + encodeURIComponent(keywords) + ")";
 
                 GM_xmlhttpRequest({
                     method: 'GET',
@@ -67,11 +67,11 @@
 
                         if (solutionsResponse.status == 401) {
 
-                            mojoCache[topicId].fetchingDocuments = false;
+                            cache[topicId].fetchingDocuments = false;
 
                             var buttonId = popoverId + 'contentbutton';
 
-                            content.append(jQuery('<p>You need to be logged into <a href="http://mojo.redhat.com">Mojo</a> for this menu to work.</p>\
+                            content.append(jQuery('<p>You need to be logged into <a href="http://community.jboss.org">Jboss.org</a> for this menu to work.</p>\
                                     <div style="display:table-cell; text-align: center; vertical-align:middle; width: 746px;">\
                                         <button id="' + buttonId + '" style="margin-top: 32px;" type="button" class="btn btn-danger">Try Again</button>\
                                     </div>'));
@@ -85,7 +85,7 @@
 
                             for (var documentIndex = 0, documentCount = documents.list.length; documentIndex < documentCount; ++documentIndex) {
                                 var document = documents.list[documentIndex];
-                                if (document.type == "document") {
+                                if (document.type == "document" || document.type == "message") {
                                     var views = '(' + document.viewCount  + (document.viewCount == 1 ? ' view' : ' views') + ')';
                                     documentsTable += '<li><a href="' + document.resources.html.ref + '">' + document.subject + ' - ' + document.author.name.givenName + ' ' + document.author.name.familyName + ' ' + views + '</a></li>';
                                 }
@@ -94,7 +94,7 @@
                             documentsTable += "</ul>";
 
                             // keep a copy of the results
-                            mojoCache[topicId].text = documentsTable;
+                            cache[topicId].text = documentsTable;
 
                             content.append(jQuery(documentsTable));
 
@@ -106,19 +106,19 @@
 
         // listen for the kcs popover
         jQuery(window).bind("jboss_opened", function(event){
-            if (!mojoCache[unsafeWindow.eventDetails.topicId]) {
+            if (!cache[unsafeWindow.eventDetails.topicId]) {
 
-                mojoCache[unsafeWindow.eventDetails.topicId] = {contentFixed: true};
+                cache[unsafeWindow.eventDetails.topicId] = {contentFixed: true};
 
                 var content = jQuery('#' + unsafeWindow.eventDetails.popoverId + "content");
                 content.empty();
 
                 var buttonId = unsafeWindow.eventDetails.popoverId + 'contentbutton';
 
-                content.append(jQuery('<p>This popover displays Mojo documents that match the keywords in the topic.</p>\
-                        <p>To use this menu you first need to log into into <a href="http://mojo.redhat.com">Mojo</a>.</p>\
+                content.append(jQuery('<p>This popover displays jboss.org documents that match the keywords in the topic.</p>\
+                        <p>To use this menu you first need to log into into <a href="http://community.jboss.org">Jboss.org</a>.</p>\
                         <div style="display:table-cell; text-align: center; vertical-align:middle; width: 746px;">\
-                            <button id="' + buttonId + '" style="margin-top: 32px;" type="button" class="btn btn-primary">Get Documents</button>\
+                            <button id="' + buttonId + '" style="margin-top: 32px;" type="button" class="btn btn-primary">Get Content</button>\
                         </div>'));
 
                 addClickFunction(buttonId, unsafeWindow.eventDetails.topicId, unsafeWindow.eventDetails.popoverId);
