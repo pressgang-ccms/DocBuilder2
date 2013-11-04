bbfe1e9d3c300a38f8d268e4bfc2f1cf6e5fb117
Matthew Casperson <mcaspers@redhat.com>
2013-11-04 13:48:06 +1000

    Working on product pages integration

--- javascript/impact.js
+++ javascript/impact.js
@@ -7,8 +7,8 @@ var PRESSGANG_TIMELINE_ITEM_HEIGHT = 10;
 var process = function (json) {
     var x = 0,
         r = Raphael("chart", window.innerWidth, 150),
-        labels = {},
-        textattr = {"font": '9px "Arial"', stroke: "none", fill: "#fff"},
+        //labels = {},
+        //textattr = {"font": '9px "Arial"', stroke: "none", fill: "#fff"},
         pathes = {},
         lgnd2 = $("#legend2")[0],
         usrnm2 = $("#username2")[0];
@@ -66,7 +66,7 @@ var process = function (json) {
                     p = pathes[processes[i]] = {f:[], b:[]};
                 }
                 // push an array with x (x pos), h (height) and the number of commits
-                p.f.push([x, h, 1]);
+                p.f.push([x, h]);
                 // add the x (x pos) and a height based on the number of commits to the start of the array
                 p.b.unshift([x, h += PRESSGANG_TIMELINE_ITEM_HEIGHT]);
                 h += 2;
@@ -79,12 +79,12 @@ var process = function (json) {
         }
         var c = 0;
         for (var i in pathes) {
-            labels[i] = r.set();
+            //labels[i] = r.set();
             var clr = Raphael.getColor();
             pathes[i].p = r.path().attr({fill: clr, stroke: clr});
             var path = "M".concat(pathes[i].f[0][0], ",", pathes[i].f[0][1], "L", pathes[i].f[0][0] + 50, ",", pathes[i].f[0][1]);
             var th = Math.round(pathes[i].f[0][1] + (pathes[i].b[pathes[i].b.length - 1][1] - pathes[i].f[0][1]) / 2 + 3);
-            labels[i].push(r.text(pathes[i].f[0][0] + 25, th, pathes[i].f[0][2]).attr(textattr));
+            //labels[i].push(r.text(pathes[i].f[0][0] + 25, th, pathes[i].f[0][2]).attr(textattr));
             var X = pathes[i].f[0][0] + 50,
                 Y = pathes[i].f[0][1];
             for (var j = 1, jj = pathes[i].f.length; j < jj; j++) {
@@ -94,7 +94,7 @@ var process = function (json) {
                 path = path.concat(X - 20, ",", Y, ",", X, ",", Y, "L", X += 50, ",", Y);
                 th = Math.round(Y + (pathes[i].b[pathes[i].b.length - 1 - j][1] - Y) / 2 + 3);
                 if (th - 9 > Y) {
-                    labels[i].push(r.text(X - 25, th, pathes[i].f[j][2]).attr(textattr));
+                    //labels[i].push(r.text(X - 25, th, pathes[i].f[j][2]).attr(textattr));
                 }
             }
             path = path.concat("L", pathes[i].b[0][0] + 50, ",", pathes[i].b[0][1], ",", pathes[i].b[0][0], ",", pathes[i].b[0][1]);
@@ -102,17 +102,17 @@ var process = function (json) {
                 path = path.concat("C", pathes[i].b[j][0] + 70, ",", pathes[i].b[j - 1][1], ",", pathes[i].b[j][0] + 70, ",", pathes[i].b[j][1], ",", pathes[i].b[j][0] + 50, ",", pathes[i].b[j][1], "L", pathes[i].b[j][0], ",", pathes[i].b[j][1]);
             }
             pathes[i].p.attr({path: path + "z"});
-            labels[i].hide();
+            //labels[i].hide();
             var current = null;
             (function (i) {
                 pathes[i].p.mouseover(function () {
                     if (current != null) {
-                        labels[current].hide();
+                        //labels[current].hide();
                     }
                     current = i;
-                    labels[i].show();
+                    //labels[i].show();
                     pathes[i].p.toFront();
-                    labels[i].toFront();
+                    //labels[i].toFront();
                     usrnm2.innerHTML = json.authors[i].n + " <em>(" + json.authors[i].c + " commits, " + json.authors[i].a + " additions, " + json.authors[i].d + " deletions)</em>";
                     lgnd2.style.backgroundColor = pathes[i].p.attr("fill");
                 });
