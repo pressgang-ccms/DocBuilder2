64c23fb92ec98ed34252d7c06761988b0d8f4f4b
Matthew Casperson <mcaspers@redhat.com>
2013-11-04 16:03:18 +1000

    Working on product pages integration

--- presszilla/PressZillaProductPagesIntegration_v3.0.3.js
+++ presszilla/PressZillaProductPagesIntegration_v3.0.3.js
@@ -146,7 +146,7 @@
                     var textElement = timelineChart.text(x + 25, h + 10, dtext).attr({"font": '9px "Arial"', stroke: "none", fill: "#aaa"});
 
                     if (dt < today) {
-                        textElement = textElement;
+                        todayOnGraph = textElement;
                     }
 
                     x += 100;
