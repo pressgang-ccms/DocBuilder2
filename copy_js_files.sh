9039b8f66c5fc125c9d055836e315f9172997fe0
Matthew Casperson <mcaspers@redhat.com>
2013-11-07 07:23:25 +1000

    Added help overlay

--- copy_js_files.sh
+++ copy_js_files.sh
@@ -1,4 +1,4 @@
-cp -f javascript/overlay.js /var/www/html/javascript
+cp -f javascript/*.js /var/www/html/javascript
 cp -f functions-1.1.js /var/www/html
 cp -f PressZilla.user.js /var/www/html
 cp -r presszilla /var/www/html
