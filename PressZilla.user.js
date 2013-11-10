ba7a723ac3e9e7ab24350e4a33bbed8cd4458aa8
Matthew Casperson <mcaspers@redhat.com>
2013-11-10 18:24:41 +1000

    Working on automatically mapping product and versions to product pages ids

--- PressZilla.user.js
+++ PressZilla.user.js
@@ -17,8 +17,8 @@
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaJbossOrgIntegration_v2.6.8.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaBugzillaSearchIntegration_v3.4.5.js
 // @require		http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaPnTIntegration_v2.7.4.js
-// @require		http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaProductPagesIntegration_v3.6.7.js
-// @version     3.6.7
+// @require		http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaProductPagesIntegration_v3.6.8.js
+// @version     3.6.8
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
 // @downloadURL https://docbuilder.usersys.redhat.com/PressZilla.user.js
