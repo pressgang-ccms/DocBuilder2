46f775a01ca47c8e685749006a1237d52ff17982
Matthew Casperson <mcaspers@redhat.com>
2013-11-18 11:20:32 +1000

    Fixing spell checking

--- javascript/overlay.js
+++ javascript/overlay.js
@@ -1257,7 +1257,7 @@ function checkSpellingErrors(dictionary, topics, index, spellingErrors, buttons)
                 }
 
                 // remove anything that does not contain a letter
-                var numberRe = /\b[^A-Za-z\s]*\b/;
+                var numberRe = /\b[^A-Za-z\s]+\b/;
                 var numberMatch = null;
                 while ((numberMatch = text.match(numberRe)) != null) {
                     var numberLength = numberMatch[0].length;
