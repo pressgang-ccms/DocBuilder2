0bb293d12da0d0fc746f55a9ab8333f1a81af225
Matthew Casperson <mcaspers@redhat.com>
2013-11-24 13:25:25 +1000

    Started work on dictionary user script

--- presszilla/PressZillaDictionary_v1.5.6.js
+++ presszilla/PressZillaDictionary_v1.5.6.js
@@ -62,6 +62,8 @@ jQuery( document ).ready(function() {
     var texts = [];
     collectTextNodes(document.body, texts);
 
+    console.log("collectTextNodes() done");
+
     var customDicUrl = SERVER + "/topics/get/json/query;propertyTagExists" + VALID_WORD_EXTENDED_PROPERTY_TAG_ID + "=true;propertyTagExists" + INVALID_WORD_EXTENDED_PROPERTY_TAG_ID + "=true;propertyTagExists" + DISCOURAGED_WORD_EXTENDED_PROPERTY_TAG_ID + "=true;propertyTagExists" + DISCOURAGED_PHRASE_EXTENDED_PROPERTY_TAG_ID + "=true;logic=Or?expand=%7B%22branches%22%3A%5B%7B%22trunk%22%3A%22topics%22%2C%22branches%22%3A%5B%7B%22trunk%22%3A%7B%22name%22%3A+%22properties%22%7D%7D%5D%7D%5D%7D";
 
     setTimeout(function() {
