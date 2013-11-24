f3759516a7e66b4d03097e0954e591d7976ff85f
Matthew Casperson <mcaspers@redhat.com>
2013-11-24 15:15:44 +1000

    Started work on dictionary user script

--- PressZillaDictionary.user.js
+++ PressZillaDictionary.user.js
@@ -5,6 +5,8 @@
 // @author      Matthew Casperson
 // @include     https://mojo.redhat.com/*
 // @include     https://access.redhat.com/*
+// @include     http://documentation-devel.engineering.redhat.com/docs
+// @include     https://documentation-devel.engineering.redhat.com/docs
 // @require     http://code.jquery.com/jquery-2.0.3.min.js
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaDictionary_v1.5.8.js
 // @require     http://docbuilder.usersys.redhat.com/22516/html/files/pressgang_website.js
