3247005e07a86f842e66b0cb1c97ff4e7f1bafdf
Matthew Casperson <mcaspers@redhat.com>
2013-11-24 13:05:58 +1000

    Started work on dictionary user script

--- presszilla/PressZillaDictionary_v1.5.1.js
+++ presszilla/PressZillaDictionary_v1.5.1.js
@@ -188,7 +188,7 @@ unsafeWindow.displayDictionaryTopic = function(topicId) {
         for (var dicIndex = 0, dicCount = dictionaryBookTopics.length; dicIndex < dicCount; ++dicIndex) {
             var dicItem = dictionaryBookTopics[dicIndex];
             if (dicItem.topicId == topicId) {
-                window.open("http://docbuilder.ecs.eng.bne.redhat.com/22516/html/" + dicItem.target + ".html","PressGangDictWindow","config='toolbar=no, menubar=no,scrollbars=no,resizable=no,location=no,directories=no,atus=no'");
+                window.open("http://docbuilder.ecs.eng.bne.redhat.com/22516/html/" + dicItem.target + ".html","PressGangDictWindow","config='toolbar=no, menubar=no,scrollbars=no,resizable=no,location=no,directories=no,atus=no,width=400,height=300'");
                 break;
             }
         }
