facedefbd48057a3232c74e2ee6da73475b199bb
Matthew Casperson <mcaspers@redhat.com>
2013-12-03 09:48:58 +1000

    Working on displaying duplicate topics

--- javascript/overlay.js
+++ javascript/overlay.js
@@ -2804,7 +2804,7 @@ function getInfoFromREST() {
                         }
 
                         // Finally we need to get the list of any similar topics
-                        var similarTopicsUrl = "http://skynet-dev.usersys.redhat.com:8080/pressgang-ccms/rest/1/topics/get/json/query;minHash=" + topicNode.entityId + "%3A0.6?expand=%7B%22branches%22%3A%5B%7B%22trunk%22%3A%22topics%22%7D%5D%7D";
+                        /*var similarTopicsUrl = "http://skynet-dev.usersys.redhat.com:8080/pressgang-ccms/rest/1/topics/get/json/query;minHash=" + topicNode.entityId + "%3A0.6?expand=%7B%22branches%22%3A%5B%7B%22trunk%22%3A%22topics%22%7D%5D%7D";
                         jQuery.getJSON(similarTopicsUrl, function(data){
                             if (!dupTopicsCache[topicNode.entityId].data) {
                                 dupTopicsCache[topicNode.entityId].data = [];
@@ -2815,7 +2815,7 @@ function getInfoFromREST() {
                             }
 
                             updateCount(topicNode.entityId + "duplicateIcon", dupTopicsCache[topicNode.entityId].data.length);
-                        });
+                        });*/
                     }
 
                     // for each topic we need the latest revision, and the specific revision included in the spec (if the revision is defined)
