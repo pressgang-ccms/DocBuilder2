7a125835f01ee17383464e73d0e9c34c9a5c22d8
Matthew Casperson <mcaspers@redhat.com>
2013-12-09 13:15:17 +1000

    Started work on PressZilla link checker

--- presszilla/PressZillaLinkChecker_v3.9.9.js
+++ presszilla/PressZillaLinkChecker_v3.9.9.js
@@ -98,7 +98,7 @@ function checkDeadLinks(links, index) {
                 url: href,
                 timeout: 10000,
                 onabort: function(link, href) { return function() {logURLNotLoading(link, href); checkDeadLinks(links, ++index);}}(link, href),
-                onerror: function(link, href) { return function() {logURLNotLoading(link, href); }}(link, href),
+                onerror: function(link, href) { return function() {logURLNotLoading(link, href); /* onload will be called anyway */}}(link, href),
                 ontimeout: function(link, href) { return function() {logURLNotLoading(link, href); checkDeadLinks(links, ++index);}}(link, href),
                 onload: function(response) {
                     checkDeadLinks(links, ++index);
