a097a429d985a89d91c914e6e7f17118dfc84f13
Matthew Casperson <mcaspers@redhat.com>
2014-01-29 11:17:40 +1000

    Working on adding support for docs devel

--- PressZillaDocsStage.user.js
+++ PressZillaDocsStage.user.js
@@ -3,10 +3,11 @@
 // @namespace   https://www.jboss.org/pressgang
 // @description PressGang BugZilla customization
 // @author      Matthew Casperson
+// @include     https://bugzilla.redhat.com/*
 // @include     http://documentation-devel.engineering.redhat.com/site/documentation/*
 // @include     https://documentation-devel.engineering.redhat.com/site/documentation/*
 // @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaCommonUtils_v2.1.1.js
-// @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaBugzillaIntegration_v3.8.0.js
+// @require     http://docbuilder.ecs.eng.bne.redhat.com/presszilla/PressZillaBugzillaIntegration_v3.8.3.js
 // @version     4.0.5
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
