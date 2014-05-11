f0b00a4bf1bd08a99b305f3a5494c9447eb068f8
Matthew Casperson <matthewcasperson@gmail.com>
2014-05-12 06:43:52 +1000

    Updated presszilla

--- copy_js_files.sh
+++ copy_js_files.sh
@@ -1,5 +1,6 @@
 cp -f javascript/*.js /var/www/html/javascript
 cp -f functions-1.1.js /var/www/html
+cp -f functions-1.2.js /var/www/html
 cp -f PressZilla.user.js /var/www/html
 cp -f PressZillaDictionary.user.js /var/www/html
 cp -r presszilla /var/www/html
